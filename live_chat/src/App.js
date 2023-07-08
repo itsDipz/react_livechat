9835d9c7afc133302ffeda108cd9bbc63568e68e
itsDipsy <elliot.collinszcv@gmail.com>
2023-07-08 15:18:55 +0200

    uppdate

---
+++ live_chat/src/App.js
@@ -0,0 +1,16 @@
+import react from "react";
+import react_dom from "react-dom";
+import { createRoot } from 'react-dom/client';
+
+function test_button(){
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+const domRoot = document.getElementById('root');
+const root = createRoot(domNode);
+
+
+
+console.log("hello");
